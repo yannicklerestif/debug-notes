d8c6fa984513cc886e611e709d3369a14e18fbb9
Yannick le Restif <y.lerestif@criteo.com>
2022-04-04 13:13:14 +0200

    JCEF (web view inside the tool window) bootstrap
    Change-Id: Ic8b6ec4377b85b02400ca661821b2c5de2b49300

---
+++ plugin/rider/DebugNotes/src/rider/main/java/org/debugnotes/toolWindow/DebugNotesToolWindow.java
@@ -0,0 +1,25 @@
+package org.debugnotes.toolWindow;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Disposer;
+import com.intellij.ui.jcef.JBCefBrowser;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+
+public class DebugNotesToolWindow {
+
+
+    private final JComponent content;
+
+    public DebugNotesToolWindow(@NotNull Project project) {
+        JBCefBrowser browser = new JBCefBrowser();
+        browser.loadURL("https://yannicklerestif.com/debug-notes");
+        Disposer.register(project, browser);
+        content = browser.getComponent();
+    }
+
+    public JComponent getContent() {
+        return content;
+    }
+}
---
+++ plugin/rider/DebugNotes/src/rider/main/java/org/debugnotes/toolWindow/DebugNotesToolWindowFactory.java
@@ -0,0 +1,25 @@
+package org.debugnotes.toolWindow;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowFactory;
+import com.intellij.ui.content.Content;
+import com.intellij.ui.content.ContentFactory;
+import org.debugnotes.toolWindow.DebugNotesToolWindow;
+import org.jetbrains.annotations.NotNull;
+
+public class DebugNotesToolWindowFactory implements ToolWindowFactory {
+
+    /**
+     * Create the tool window content.
+     *
+     * @param project    current project
+     * @param toolWindow current tool window
+     */
+    public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
+        DebugNotesToolWindow myToolWindow = new DebugNotesToolWindow(project);
+        ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
+        Content content = contentFactory.createContent(myToolWindow.getContent(), "", false);
+        toolWindow.getContentManager().addContent(content);
+    }
+}
