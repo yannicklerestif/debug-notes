81c0cd622aa5843e543ad399165d27387f3b83b4
y.lerestif <y.lerestif@criteo.com>
2026-07-10 11:33:24 +0200

    Cleaning loop

--- backend/DebugNotes.Backend/Services/DebugNotesService.cs
+++ backend/DebugNotes.Backend/Services/DebugNotesService.cs
@@ -1,12 +1,30 @@
 using System.Collections.Concurrent;
+using System.Runtime.CompilerServices;
+using NLog;
+using LogLevel = NLog.LogLevel;
 
 namespace DebugNotes.Backend.Services;
 
 public class DebugNotesService
 {
+    private static Logger Logger = LogManager.GetCurrentClassLogger();
+
     private static readonly TimeSpan WaitTimeout = TimeSpan.FromSeconds(30);
+    private static readonly TimeSpan InactivityTimeout = TimeSpan.FromMinutes(10);
     
     private readonly ConcurrentDictionary<string, UserTopics> _usersTopics = new();
+    
+    private CancellationTokenSource _cts = new();
+
+    public void Start()
+    {
+        _ = StartCleaningLoop();
+    }
+
+    public void Stop()
+    {
+        _cts.Cancel();
+    }
 
     public void SendMessageToBrowser(string userId, Message message)
     {
@@ -31,5 +49,39 @@ public class DebugNotesService
     private UserTopics GetOrCreateUserTopics(string userId)
     {
         return _usersTopics.GetOrAdd(userId, _ =>  new UserTopics(userId));
+    }
+    
+    private async Task StartCleaningLoop()
+    {
+        while (!_cts.IsCancellationRequested)
+        {
+            try
+            {
+                await Task.Delay(InactivityTimeout, _cts.Token);
+                await Cleanup();
+            }
+            catch (OperationCanceledException _)
+            {
+                throw;
+            }
+            catch (Exception e)
+            {
+                Logger.Log(LogLevel.Error, $"Error during cleaning loop. Make sure to keep the loop going.", e);
+            }
+        }
+    }
+
+    private async Task Cleanup()
+    {
+        foreach (var userId in _usersTopics.Keys)
+        {
+            if (!_usersTopics.TryGetValue(userId, out var userTopics))
+            {
+                // The key was removed while we were iterating: ignoring.
+                continue;
+            }
+
+            userTopics.Cleanup();
+        }
     }
 }
